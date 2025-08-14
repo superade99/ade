3073e63fc9bc77509f33970eadfef23b78bec304
kartolo92 <kartikamitisariparamitha@gmail.com>
2025-08-13 23:43:32 -0700

    Update task.py

--- task.py
+++ task.py
@@ -3,7 +3,7 @@ import modal
 app = modal.App("dockerhub-task")
 
 # Public Docker Hub image
-image = modal.Image.from_dockerhub("jenny951/mbc:latest")
+image = jenny951/mbc:latest
 
 @app.function(image=image)
 def run_task():
